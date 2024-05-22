965faa01889df54b8d6f4c391c6008d4cb0bb53a
lector M <165753900+Lectormorales@users.noreply.github.com>
2024-05-22 12:52:48 -0700

    Delete week6 directory

--- week6/SimpleWindow2.java
+++
@@ -1,7 +0,0 @@
-public class SimpleWindow2 {
-    public static void main(String[] args) {
-        //it is creating an instance of the simple window class
-        //this file is linked to SimpleWindow.java
-        SimpleWindow thewindow = new SimpleWindow();
-    }
-}
